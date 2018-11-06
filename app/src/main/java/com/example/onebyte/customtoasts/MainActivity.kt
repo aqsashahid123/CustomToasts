21a83abe5237d00b8fe3ea6ad93df9f456050506
Aqsa <aqsa@onebyte.biz>
2018-11-06 18:13:41 +0500

    commit by aqsa ==>>>> TEST TOAST LIBRARY

--- app/src/main/java/com/example/onebyte/customtoasts/MainActivity.kt
+++ app/src/main/java/com/example/onebyte/customtoasts/MainActivity.kt
@@ -1,27 +1,75 @@
 package com.example.onebyte.customtoasts
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
-import com.example.customtoasts2.ToastCreater
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.zip.Inflater
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    var context : Context = this
+    var context: Context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         context = this
+//        System.loadLibrary("customtoasts2")
         btnRandom.setOnClickListener(this)
+        btnColouredToast.setOnClickListener(this)
 
     }
+
     override fun onClick(p0: View?) {
-        if (p0===btnRandom){
-            ToastCreater.simpleToast("AQSAAA", context,true )
+        if (p0 === btnRandom) {
+            // ToastCreater.simpleToast("AQSAAA", context,true )
+        } else if (p0 === btnColouredToast) {
+            var toast: Toast = Toast(context)
+            toast.createToast(context,"Finally :)",Gravity.BOTTOM,"#000000",0)
+            //toastWithBackgroundColor("AQSAAA", context, true, Color.BLACK)
         }
     }
 
+//    fun toastWithBackgroundColor(message: String, context: Context, isLong: Boolean, color: Int) {
+//        try {
+//            var toast: Toast
+//            if (isLong) {
+//                toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+//            } else {
+//                toast = Toast.makeText(context, message, Toast.LENGTH_SHORT)
+//
+//            }
+//            toast.view.background.colorFilter = PorterDuffColorFilter(0xffff00, PorterDuff.Mode.MULTIPLY)
+//            //view.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_IN)
+//            toast.show()
+//        } catch (exception: Exception) {
+//            Log.e("Error Occured", "Invalid Color")
+//        }
+//
+//    }
+
+    fun Toast.createToast(context: Context,message: String,gravity: Int,
+                          backgroundColor: String,imageBackgrount: Int){
+        val infflater: LayoutInflater= context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val layout = infflater.inflate(R.layout.custom_toast, (context as Activity).findViewById(R.id.root))
+        layout.findViewById<TextView>(R.id.customToastText).text = message
+        layout.setBackgroundColor(Color.parseColor(backgroundColor))
+        setGravity(gravity,0,100)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+
+    }
 }
--- customtoasts2/src/main/java/com/example/customtoasts2/ToastCreater.kt
+++ customtoasts2/src/main/java/com/example/customtoasts2/ToastCreater.kt
@@ -5,8 +5,11 @@ import android.widget.Toast
 import android.widget.TextView
 import android.graphics.PorterDuff
 import android.R.attr.duration
+import android.app.Activity
 import android.graphics.Color
 import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
 
 
 class ToastCreater {
@@ -19,23 +22,47 @@ class ToastCreater {
             }
         }
 
-        fun toastWithBackgroundColor(message: String, context: Context, isLong: Boolean, color: Int) {
-            try {
-                var toast: Toast
-                if (isLong) {
-                    
-                    toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
-                }else{
-                    toast = Toast.makeText(context, message, Toast.LENGTH_SHORT)
+//        fun toastWithBackgroundColor(message: String, context: Context, isLong: Boolean, color: Int) {
+//            try {
+//                var toast: Toast
+//                if (isLong) {
+//
+//                    toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+//                }else{
+//                    toast = Toast.makeText(context, message, Toast.LENGTH_SHORT)
+//
+//                }
+//                val view = toast.getView()
+//                view.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_IN)
+//                toast.show()
+//            }catch (exception: Exception){
+//                Log.e("Error Occured", "Invalid Color")
+//            }
+//
+//        }
 
-                }
-                val view = toast.getView()
-                view.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_IN)
-                toast.show()
-            }catch (exception: Exception){
-                Log.e("Error Occured", "Invalid Color")
-            }
+        fun createToastWithCustomLayout(context: Context,message: String,gravity: Int,
+                                        backgroundColor: String,imageBackgrount: Int,
+                                        layout: Int, idOfRootInLayout: Int, idOfTextView: Int){
+            var toast: Toast = Toast(context)
+            toast.createToast(context,message,gravity,
+                    backgroundColor, layout, idOfRootInLayout, idOfTextView)
+        }
+       private fun Toast.createToast(context: Context,message: String,gravity: Int,
+                              backgroundColor: String, layout: Int, idOfRootInLayout: Int, idOfTextView: Int){
+            val infflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val layout = infflater.inflate(layout, (context as Activity).findViewById(idOfRootInLayout))
+            layout.findViewById<TextView>(idOfTextView).text = message
+            layout.setBackgroundColor(Color.parseColor(backgroundColor))
+            setGravity(gravity,0,100)
+            duration = Toast.LENGTH_LONG
+            view = layout
+            show()
 
         }
+
+
+
     }
+
 }
